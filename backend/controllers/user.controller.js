dfca0959a94e29fd8823573edd413bcbc82b658a
scastillonu@uniminuto.edu.co <scastillonu@uniminuto.edu.co>
2021-02-10 00:04:16 -0500

    endpoint user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,54 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const saltRounds = 10;
+const userCtrl = {};
 
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
- 
-const userCtrl = {}
- 
 userCtrl.signUp = (req, res) => {
-    console.log('End Point');
-}
- 
+  const user = new User(); //Instancia del modelo User
+  const { name, lastname,email, password, repeatPassword } = req.body; //Treaemos los datos que vienen de la vista en html
+  user.name=name;
+  user.lastname=lastname;
+  user.email = email; //La propiedad user.email que vienen del modelo se iguala o se asigna a email, que viene del html
+  user.role = "Admin";
+  user.active = true;
+  if (!password || !repeatPassword) {
+    res.status(404).send({
+      message: "Las contraseñas son obligatorias",
+    });
+  } else {
+    if (password !== repeatPassword) {
+      res.status(404).send({
+        message: "Claves no son iguales",
+      });
+    } else {
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        if (err) {
+          res.status(500).send({
+            message: "Error al encriptar la contraseña",
+          });
+        } else {
+          user.password = hash;
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({
+                message: "El usuario ya existe",
+              });
+            } else {
+              if (!userStored) {
+                res.status(400).send({
+                  message: "Error al crear el usuario",
+                });
+              } else {
+                res.status(200).send({
+                  user: userStored,
+                });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
+};
+
 module.exports = userCtrl;
