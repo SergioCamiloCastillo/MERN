7976b0c6e52efa6bd293d773c656345d79fa481f
desarrollo6 <desarrollo6@omegawebsystems.com>
2021-03-24 21:41:24 -0500

    listar usuarios

--- client/src/components/Admin/Users/ListUsers.jsx
+++ client/src/components/Admin/Users/ListUsers.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from 'react';
 import { Switch, List, Avatar, Button } from "antd";
-import noavatar from "../../../assets/img/png/no-avatar.png";
+import Noavatar from "../../../assets/img/png/no-avatar.png";
 import "./ListUsers.scss";
 export default function ListUsers(props) {
     const { usersActive, usersInactive } = props;
     const [viewUserActive, setviewUserActive] = useState(true);
     console.log(usersActive);
     console.log(usersInactive);
+
     return (<div className='list-users'><div className='list-users__switch'>
         <Switch defaultChecked onChange={() => setviewUserActive(!viewUserActive)}></Switch>
         <span>{viewUserActive ? "Usuarios Activos" : "Usuarios Inactivos"}</span>
     </div>
-    {viewUserActive?<UserActive></UserActive>:<UserInactive></UserInactive>}
+        {viewUserActive ? <UserActive></UserActive> : <UserInactive></UserInactive>}
     </div>)
 }
-function UserActive(){
-    return <h4>Uusario lista activos</h4>
+function UserActive(props) {
+    const { usersActive } = props;
+
+    return (
+        <List className='users-active'
+            itemLayout='horizontal'
+            dataSource={usersActive}
+            renderItem={user => (
+                <List.Item>
+
+                    <List.Item.Meta
+                        avatar={<Avatar src={user.avatar ? user.avatar : Noavatar} />}
+                        title={`
+                ${user.name ? user.name : "..."} 
+                ${user.lastname ? user.lastname : "..."}
+            `}
+                        description={user.email}
+                    />
+                </List.Item>
+            )}
+        ></List>
+    );
+
 }
-function UserInactive(){
+function UserInactive() {
     return <h4>Uusario lista Inactivos</h4>
 }
--- client/src/pages/Admin/Users/Users.jsx
+++ client/src/pages/Admin/Users/Users.jsx
@@ -3,25 +3,33 @@ import {getAccessTokenApi} from '../../../api/auth';
 import {getUserActiveApi} from '../../../api/user';
 import "./Users.scss";
 import ListUsers from "../../../components/Admin/Users/ListUsers.jsx";
+
+
+import "./Users.scss";
+
 export default function Users() {
   const [usersActive, setUsersActive] = useState([]);
   const [usersInactive, setUsersInactive] = useState([]);
-
+  const [reloadUsers, setReloadUsers] = useState(false);
   const token = getAccessTokenApi();
 
-
   useEffect(() => {
-    getUserActiveApi(token, true).then(response=>{
-      setUsersActive(response);
+    getUserActiveApi(token, true).then((response) => {
+      setUsersActive(response.users);
     });
-    getUserActiveApi(token, false).then(response=>{
-      setUsersInactive(response);
+    getUserActiveApi(token, false).then((response) => {
+      setUsersInactive(response.users);
     });
-  
-  }, [token]);
+    setReloadUsers(false);
+  }, [token, reloadUsers]);
+
   return (
-    <div className='users'>
-     <ListUsers usersActive={usersActive} usersInactive={usersInactive}></ListUsers>
+    <div className="users">
+      <ListUsers
+        usersActive={usersActive}
+        usersInactive={usersInactive}
+        setReloadUsers={setReloadUsers}
+      />
     </div>
   );
 }
