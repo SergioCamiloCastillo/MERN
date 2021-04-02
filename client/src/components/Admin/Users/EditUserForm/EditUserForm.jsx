23f3d71adae633d18636113277353f4a0acee4df
desarrollo6 <desarrollo6@omegawebsystems.com>
2021-04-02 16:25:05 -0500

    edit user

--- client/src/components/Admin/Users/EditUserForm/EditUserForm.jsx
+++ client/src/components/Admin/Users/EditUserForm/EditUserForm.jsx
@@ -1,5 +1,9 @@
 import React, { useCallback, useState } from 'react';
 import "./UserUserForm.scss";
+import {
+    UserOutlined,MailOutlined
+
+} from "@ant-design/icons";
 import { Avatar, Form, Input, Select, Button, Col, Row } from "antd";
 import { useDropzone } from 'react-dropzone';
 import NoAvatar from "../../../../assets/img/png/no-avatar.png";
@@ -7,17 +11,20 @@ export default function EditUserForm(props) {
     const { user } = props;
     const [avatar, setAvatar] = useState(null);
     const [userData, setUserData] = useState({
-        name:user.name,
-        lastname:user.lastname,
+        name: user.name,
+        lastname: user.lastname,
         email: user.email,
-        role:user.role,
-        avatar:user.avatar
+        role: user.role,
+        avatar: user.avatar
     });
+    const updateUser = e => {
+        console.log(userData);
+    }
     return (
 
         <div className='edit-user-form'>
-            <UploadAvatar  avatar={avatar} setAvatar={setAvatar}></UploadAvatar>
-            <EditForm user={user}/>
+            <UploadAvatar avatar={avatar} setAvatar={setAvatar}></UploadAvatar>
+            <EditForm userData={userData} setUserData={setUserData} updateUser={updateUser} />
         </div>
     )
 }
@@ -35,24 +42,74 @@ function UploadAvatar(props) {
         noKeyboard: true,
         onDrop
     });
-    
+
     return (
         <div className="upload-avatar" {...getRootProps()}>
-          <input {...getInputProps()} />
-          {isDragActive ? (
-            <Avatar size={150} src={NoAvatar} />
-          ) : (
-            <Avatar size={150} src={avatar ? avatar.preview : NoAvatar} />
-          )}
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <Avatar size={150} src={NoAvatar} />
+            ) : (
+                <Avatar size={150} src={avatar ? avatar.preview : NoAvatar} />
+            )}
         </div>
-      );
+    );
 }
-function EditForm(props){
-    const {user, userData, setUserData, updateUser} = props;
+function EditForm(props) {
+    const { userData, setUserData, updateUser } = props;
+    const { Option } = Select;
+
     return (
-        <div>
-            <h2>Form...</h2>
-        </div>
+        <Form className='form-edit' onFinish={updateUser}>
+            <Row gutter={24}>
+                <Col span={12}>
+                    <Form.Item>
+                        <Input
+                            prefix={
+                                <UserOutlined></UserOutlined>
+                            }
+
+                            defaultValue={userData.name}
+                            placeholder="Nombres"
+                            onChange={e => setUserData({ ...userData, name: e.target.value })}
+                        ></Input>
+                    </Form.Item>
+                </Col>
+                <Col span={12}>
+                    <Form.Item>
+                        <Input
+                            prefix={
+                                <UserOutlined></UserOutlined>
+                            }
+
+                            defaultValue={userData.lastname}
+                            placeholder="Apellidos"
+                            onChange={e => setUserData({ ...userData, lastname: e.target.value })}
+                        ></Input>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row gutter={24}>
+                <Col span={12}>
+                    <Input prefix={<MailOutlined />} defaultValue={userData.email}
+                        placeholder='Correo Electrónico'
+                        onChange={e => setUserData({ ...userData, email: e.target.value })}
+                    >
+                    </Input>
+                </Col>
+                <Col span={12}></Col>
+
+            </Row>
+            <Row gutter={24}>
+                <Col span={12}></Col>
+                <Col span={12}></Col>
+
+            </Row>
+            <Form.Item>
+                <Button type='primary' htmlType='submit' className='btn-submit'>
+                    Actualizar
+            </Button>
+            </Form.Item>
+        </Form>
     );
 
 }
